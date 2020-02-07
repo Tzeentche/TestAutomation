ef72d0f7cd8c7202a2c24c55b19e15677adeb8cf
Tzeentche <ilyasugako87@gmail.com>
2020-02-08 00:19:15 +0300

    Practice Set collection to TestAutomation training.

---
+++ src/SetCollection/SetCollection.java
@@ -0,0 +1,18 @@
+package SetCollection;
+
+import java.util.*;
+
+public class SetCollection {
+
+    public static void main(String[] args) {
+
+        Set<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(11);
+        set.add(5);
+        set.add(7);
+
+        System.out.println(set.size());
+
+    }
+}
