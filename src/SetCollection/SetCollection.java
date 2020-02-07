78d3cdb52162fd7e08da71dedb82788756fbb046
Tzeentche <ilyasugako87@gmail.com>
2020-02-08 00:32:57 +0300

    Finished practice with Set collection to TestAutomation training.

--- src/SetCollection/SetCollection.java
+++ src/SetCollection/SetCollection.java
@@ -9,10 +9,28 @@ public class SetCollection {
         Set<Integer> set = new HashSet<>();
         set.add(10);
         set.add(11);
-        set.add(5);
         set.add(7);
+        set.add(5);
 
         System.out.println(set.size());
 
+        set.remove(11);
+
+        Iterator<Integer> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        for (int i : set) {
+
+            System.out.println(i);
+        }
+
+//        set.clear();
+
+        System.out.println(set.size());
+
+        System.out.println(set.contains(5));
+        System.out.println(set.contains(11));
     }
 }
