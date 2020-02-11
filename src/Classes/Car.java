544ba4f4f6171085f64e5ed9121ec654f547259d
Tzeentche <ilyasugako87@gmail.com>
2020-02-11 23:45:28 +0300

    Add methods an their realisation to Classes in testing methods.

--- src/Classes/Car.java
+++ src/Classes/Car.java
@@ -1,4 +1,30 @@
 package Classes;
 
 public class Car {
+
+    int height;
+    int width;
+    int lenght;
+    int weight;
+    int maxWeight;
+    String color;
+    int speed;
+    int maxSpeed;
+
+    public void addWeight(int weight) {
+
+        this.weight += weight;
+    }
+
+    public void drive(int speed) {
+
+        if(weight <= maxWeight) {
+
+            this.speed = speed;
+
+        } else {
+
+            System.out.println("Cannot drive!");
+        }
+    }
 }
