4f0c82651ec625d35663b496f6f79e91874fa3b3
Tzeentche <ilyasugako87@gmail.com>
2020-02-12 23:55:36 +0300

    Add comments to Classes in testing methods.

--- src/Classes/ClassesAndElse.java
+++ src/Classes/ClassesAndElse.java
@@ -4,7 +4,12 @@ public class ClassesAndElse {
 
     public static void main(String[] args) {
 
+//             Create class example
+
         Car car1 = new Car();
+
+//        Set example variables:
+
         car1.color = "Black";
         car1.lenght = 5000;
         car1.height = 2000;
@@ -16,6 +21,7 @@ public class ClassesAndElse {
         car1.addWeight(700);
         car1.drive(100);
 
+//        Create new examples:
         Car car2= new Car();
         Car car3 = new Car();
         car3.color = "Red";
