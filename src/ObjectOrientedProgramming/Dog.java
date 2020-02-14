415c9950c7782ed10f6741472218ec12c74d50bc
Tzeentche <ilyasugako87@gmail.com>
2020-02-15 00:02:42 +0300

    Fill class "Dog" with methods.

--- src/ObjectOrientedProgramming/Dog.java
+++ src/ObjectOrientedProgramming/Dog.java
@@ -2,5 +2,15 @@ package ObjectOrientedProgramming;
 
 public class Dog extends Animal {
 
-    
+    int weight;
+
+    public void sit() {
+
+        System.out.println("I'm sitting!");
+    }
+
+    public void lay() {
+
+        System.out.println("I'm laying");
+    }
 }
